78ad81d50b303e42b6d398ced87f626dc678cb79
SuperSkinny11 <eversuperskinny@gmail.com>
2023-06-29 18:22:47 +0400

    some changes

--- contorllers/bookController.js
+++ contorllers/bookController.js
@@ -22,7 +22,11 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
           product_data: {
             name: `${tour.name} Tour`,
             description: tour.summary,
-            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+            images: [
+              `${req.protocol}://${req.get('host')}/img/tours/${
+                tour.imageCover
+              }`,
+            ],
           },
           unit_amount: tour.price * 100,
         },
@@ -51,7 +55,7 @@ exports.webhookCheckout = (req, res, next) => {
 
   let event;
   try {
-    event = stripe.webhooks.constructEvent(
+    event = Stripe.webhooks.constructEvent(
       req.body,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET
