13b6a8de0ab018e9ea5b3e8167c6555645885fc0
SuperSkinny11 <eversuperskinny@gmail.com>
2023-07-01 11:59:53 +0400

    some changes in app.js

--- app.js
+++ app.js
@@ -69,7 +69,16 @@ app.use(
   })
 );
 app.use(cors());
+// Access-Control-Allow-Origin *
+// api.natours.com, front-end natours.com
+app.use(
+  cors({
+    origin: 'https://checkout.stripe.com',
+  })
+);
+
 app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors());
 //development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
